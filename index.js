2fd778b0f1b5660fe0e949f88868d4b843916052
tuanlinh <tuanlinhtn2k1@gmail.com>
2022-09-25 01:07:38 +0700

    homework

---
+++ index.js
@@ -0,0 +1,7 @@
+let userName = prompt("Tên bạn là gì zậy")
+console.log(userName);
+if(userName === ("")){
+    alert("sao bạn ko nhập gì thôi kệ xem đi")
+}else{
+    alert("hello"+" " + userName + " " +"chào bạn đến với trang web của tôi")
+}
